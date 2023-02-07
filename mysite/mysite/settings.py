0281d860cecd6b8d0ad49b6b476a5db38621f8a5
MindaugasPu <mindaugas.pupienis@gmail.com>
2023-02-07 15:29:58 +0200

    grąžinau SECRET KEY į settings.py, nes bandant klonuoti nepavyko pasileist programos

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from .my_settings import SECRET_KEY
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = '$#_t(j2_i0@7vmf%c(d556$!-=*$!q7(f*8%=-&+t*wdlz0de4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
